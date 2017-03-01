e214e4e3c7dc3337cb01f80e4a37ecbf4f23516e
Pachj <Pachj@users.noreply.github.com>
2017-03-01 17:10:25 +0100

    start stop switch functionality added but not working at the moment

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
 let workLength = 25;
 let breakLength = 5;
 
+let working = true;
+let isRunning = false;
+
+/*let start = new Date();
+console.log(start);*/
 
 $(document).ready(function () {
     $("#work-length").children().click(function () {
@@ -12,7 +17,16 @@ $(document).ready(function () {
     $("#break-length").children().click(function () {
         changeBreakLength($(this).attr("value"));
     });
+    $("#start-stop").click(function () {
+        runningSwitcher();
+        /*stop();*/
+    })
 });
+
+/*function stop() {
+    let end = new Date();
+    console.log(start - end);
+}*/
 
 function changeWorkLength(operator) { //ToDo: add time maximum and minimum
     if (operator === "+") {
@@ -32,4 +46,43 @@ function changeBreakLength(operator) { //ToDo: add time maximum and minimum
         breakLength--;
     }
     $("#display-break").html(breakLength);
+}
+
+function runningSwitcher() { //ToDo: clock, Timer Object?
+    let countdown;
+    let timer;
+    let remainingInMilliseconds;
+    let start;
+
+    if (!isRunning) {
+        isRunning = true;
+        if (working) {
+            start = new Date();
+            console.log(start);
+            countdown = window.setTimeout(timeOver, workLength * 60 * 1000);
+            timer = window.setInterval(changeProgress, (workLength * 60 * 1000) / 60);
+        }
+        else {
+            countdown = window.setTimeout(timeOver, breakLength * 60 * 1000);
+            timer = window.setInterval(changeProgress, (breakLength * 60 * 1000) / 60);
+        }
+    }
+/*    else {
+        isRunning = false;
+        if (working) {
+            remainingInMilliseconds = /!*(workLength * 60 * 1000) -*!/ (Math.abs(start - new Date())); // needs to be tested
+            window.clearTimeout(countdown);
+            window.clearInterval(timer);
+            console.log((remainingInMilliseconds / 1000));
+        }
+
+    }*/
+}
+
+function timeOver() { //ToDo: change working
+
+}
+
+function changeProgress() {
+
 }
