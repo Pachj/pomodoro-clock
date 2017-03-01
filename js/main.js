d3f6fa2511b163e74139e0b9cdef951cb1e57dfe
Pachj <Pachj@users.noreply.github.com>
2017-03-01 15:56:59 +0100

    plus and minus buttons are now working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 /**
  * Created by Henry on 01.03.17.
  */
+let workLength = 25;
+let breakLength = 5;
+
 
 $(document).ready(function () {
-
+    $("#work-length").children().click(function () {
+        changeWorkLength($(this).attr("value"));
+    });
+    $("#break-length").children().click(function () {
+        changeBreakLength($(this).attr("value"));
+    });
 });
 
+function changeWorkLength(operator) { //ToDo: add time maximum and minimum
+    if (operator === "+") {
+        workLength++;
+    }
+    else {
+        workLength--;
+    }
+    $("#display-work").html(workLength);
+}
+
+function changeBreakLength(operator) { //ToDo: add time maximum and minimum
+    if (operator === "+") {
+        breakLength++;
+    }
+    else {
+        breakLength--;
+    }
+    $("#display-break").html(breakLength);
+}
